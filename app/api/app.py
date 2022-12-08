674f854fc80bc5f78c9a14fea68dba7990ffadea
Rishabh Sharma <59526954+ris3abh@users.noreply.github.com>
2022-12-07 21:33:28 -0500

    flask app developed in development mode

---
+++ app/api/app.py
@@ -0,0 +1,24 @@
+from flask import flash, Flask, jsonify, redirect
+from utilities import predict_pipeline
+
+app = Flask(__name__)
+
+
+@app.post('/predict')
+def predict():
+    data = request.json
+    try:
+        sample = data['text']
+    except KeyError:
+        return jsonify({'error': 'Missing text.'})
+
+    sample = [sample]
+    predictions = predict_pipeline(sample)
+    try:
+        result = jsonify(predictions[0])
+    except KeyError:
+        result = jsonify({'error': 'Error in prediction.'})
+    return result
+
+if __name__ == '__main__':
+    app.run(debug = True)
